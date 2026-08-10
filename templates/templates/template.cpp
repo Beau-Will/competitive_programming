574fb19b6cf8c19bd2e5cef5547051a00719f460
Beau-Will <1042383080@qq.com>
2026-08-11 05:02:55 +0800

    update template template

--- templates/templates/template.cpp
+++ templates/templates/template.cpp
@@ -1,22 +1,21 @@
 #include <bits/stdc++.h>
 
 using i64 = long long;
+using i128 = __int128;
 
 constexpr int inf1 = 1E9;
 constexpr i64 inf2 = 1E18;
 constexpr int P1 = 1E9+7, P2 = 998'244'353;
 constexpr double eps = 1E-10;
-constexpr double pi = std::numbers::pi;
-constexpr double e = std::numbers::e;
+// constexpr double pi = std::numbers::pi;
+// constexpr double e = std::numbers::e;
 
 void solve(){
   
 }
 
 int main(){
-  std::ios::sync_with_stdio(false);
-  std::cin.tie(nullptr);
-  std::cout.tie(nullptr);
+  std::cin.tie(nullptr) -> sync_with_stdio(false);
 
   int T = 1;
   // std::cin >> T;
@@ -24,6 +23,4 @@ int main(){
   for(int i = 1; i <= T; i++){
     solve();
   }
-
-  return 0;
 }
