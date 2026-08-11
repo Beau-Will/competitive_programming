9c67fef07eb483f0a1a13487760c810522c49d6c
Beau-Will <1042383080@qq.com>
2026-08-12 06:12:45 +0800

    update template template

--- templates/templates/template.cpp
+++ templates/templates/template.cpp
@@ -1,14 +1,20 @@
-#include <bits/stdc++.h>
+#include <iostream>
 
 using i64 = long long;
+using u64 = unsigned long long;
 using i128 = __int128;
+using ld = long double;
 
 constexpr int inf1 = 1E9;
 constexpr i64 inf2 = 1E18;
-constexpr int P1 = 1E9+7, P2 = 998'244'353;
+constexpr int P1 = 1'000'000'007, P2 = 998'244'353;
 constexpr double eps = 1E-10;
 // constexpr double pi = std::numbers::pi;
 // constexpr double e = std::numbers::e;
+
+// #include <limits>
+// constexpr int int_max = std::numeric_limits<int>::max();
+// constexpr i64 i64_max = std::numeric_limits<i64>::max();
 
 void solve(){
   
@@ -20,7 +26,9 @@ int main(){
   int T = 1;
   // std::cin >> T;
 
-  for(int i = 1; i <= T; i++){
+  for(int i = 1; i <= T; i++) {
     solve();
   }
+
+  return 0;
 }
