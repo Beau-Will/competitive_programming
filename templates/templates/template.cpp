a1bee7aa1a07f8c449a4ff47850c088205b3fe0e
Beau-Will <1042383080@qq.com>
2026-08-14 16:49:03 +0800

    update template template

--- templates/templates/template.cpp
+++ templates/templates/template.cpp
@@ -1,8 +1,10 @@
-#include <iostream>
+#include <bits/stdc++.h>
 
+using u32 = unsigned;
 using i64 = long long;
 using u64 = unsigned long long;
 using i128 = __int128;
+using u128 = unsigned __int128;
 using ld = long double;
 
 constexpr int inf1 = 1E9;
@@ -12,7 +14,6 @@ constexpr double eps = 1E-10;
 // constexpr double pi = std::numbers::pi;
 // constexpr double e = std::numbers::e;
 
-// #include <limits>
 // constexpr int int_max = std::numeric_limits<int>::max();
 // constexpr i64 i64_max = std::numeric_limits<i64>::max();
 
@@ -26,9 +27,5 @@ int main(){
   int T = 1;
   // std::cin >> T;
 
-  for(int i = 1; i <= T; i++) {
-    solve();
-  }
-
-  return 0;
+  for(int i = 1; i <= T; i++) solve();
 }
